42e5b58f0878494a602c2fcfa45cdc3f06d4b570
Mike de Heij <mike@deheij.email>
2016-05-28 17:04:55 +0200

    Add ING QR Code comment

--- utils/ing.go
+++ utils/ing.go
@@ -2,6 +2,9 @@ package utils
 
 //import "strings"
 
+/*GetQRCode generates the content for a ING Mobiel Bankieren QR code
+The content will be transformed into an QR image client side.
+*/
 func GetQRCode(ownerName string, ownerIBAN string, amount string) (QRCodeText string) {
 	//if strings.Contains(strings.ToUpper(debtorIBAN), "INGB") {
 	QRCodeText = "BCD\n002\n1\nSCT\n\n" + ownerName + "\n" + ownerIBAN + "\nEUR" + amount
